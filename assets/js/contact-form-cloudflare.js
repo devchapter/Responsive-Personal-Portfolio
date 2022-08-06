7f07fd38c89121183466809cd25b13431486813e
Dev Chapter <106814237+devchapter@users.noreply.github.com>
2022-08-06 22:14:12 +0430

    Add unfech polyfill and fix cors issue.

--- assets/js/contact-form-cloudflare.js
+++ assets/js/contact-form-cloudflare.js
@@ -1,6 +1,10 @@
 const TOKEN = '5478839524:AAEzSFsxINX59qPblbm54zu9XyC4F8W-nGg';
 const CHAT_ID = '1076187640';
-
+const requestHeaders = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET,POST,OPTIONS",
+    "Access-Control-Max-Age": "86400",
+}
 addEventListener('fetch', event => {
     event.respondWith(handleRequest(event.request))
 })
@@ -8,13 +12,8 @@ addEventListener('fetch', event => {
 async function handleRequest(request) {
     if (request.method == "OPTIONS") {
         return new Response(null, {
-            headers: {
-                "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Methods": "GET,POST,OPTIONS",
-                "Access-Control-Max-Age": "86400",
-            }
+            headers: requestHeaders
         })
-
     } else {
         const body = await request.json()
         const {name, email, message} = body;
@@ -37,7 +36,6 @@ async function handleRequest(request) {
         })
         const results = await response.json();
         const responseMessage = results.ok ? 'Your message has been successfully sent.' : 'An error occurred while sending the message.'
-        return new Response(responseMessage, {status: 200,})
+        return new Response(responseMessage, {status: 200, headers: requestHeaders})
     }
-
 }
